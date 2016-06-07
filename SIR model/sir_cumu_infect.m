67f8da6d85658e8135b90b7cf4def104895a9864
erinestafford <erinestafford@gmail.com>
2016-06-07 13:26:52 -0500

    fixing cumulative infected

--- SIR model/sir_cumu_infect.m
+++ SIR model/sir_cumu_infect.m
@@ -1,6 +1,7 @@
-function[cumu_inf] = sir_cumu_infect (params,out, t, init)
+function [cumu_inf] = sir_cumu_infect (params,out, t, init)
 
-out = sir_output(t, init, params);
+[t,out] = sir_output(t, init, params,[]);
+
 cumu_inf = out(end,4);
 
 end
