567004ab60bba9eee4e7c950fb6f8104d98aa1c1
erinestafford <erinestafford@gmail.com>
2016-06-07 16:49:41 -0500

    plots real data in points rather than lines

--- SIR model/plot_both.m
+++ SIR model/plot_both.m
@@ -5,7 +5,7 @@ xlabel('Time in weeks')
 ylabel('population')
 hold on
 t1 = t';
-plot(t1(1:length(data)),data, 'r');
+plot(t1(1:length(data)),data, '*');
 plot(t1(1:length(data)),Y(1:length(data),4), 'b')
 legend('Real infected count','Model infected count')
 
