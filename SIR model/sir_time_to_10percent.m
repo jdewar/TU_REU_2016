16df2538295702a9afc88bf0b38ef4a96cbf0907
erinestafford <erinestafford@gmail.com>
2016-06-07 10:35:59 -0500

    separating sensitivity analysis from Q functions

---
+++ SIR model/sir_time_to_10percent.m
@@ -0,0 +1,8 @@
+function [time] = sir_time_to_10percent(params,total_pop, t_in,init)
+
+    options = odeset('Events',@(t,Y)sir_10percentI(t, Y,total_pop));
+    fn = @(t,x)sir_rhs(t,x,params);
+    [t,y,te,ye,ie] = ode45(fn,t_in, init, options);
+    time = te;
+    
+end
