e7d0eb8f157146fec378f896d3c7f40e944ab9f5
emeyer121 <emeyer121@gmail.com>
2016-06-08 11:07:28 -0500

    changed name

--- Chikungunya Model/chik_R0_calc.m
+++ Chikungunya Model/chik_R0_calc.m
@@ -1,6 +1,5 @@
-function [R0] = R0_calc(params)
+function [R0] = chik_R0_calc(params)
 zeta = (params(10)*params(9))/(params(10)* params(12) + params(9)*params(11));
-
 R_vh = params(1) * params(11) * zeta * (params(8)/((params(7)+params(8))*params(7)));
 R_hv = params(2) * params(12) * zeta * (params(5)/((params(4)+params(5))*(params(4) + params(3))));
 
