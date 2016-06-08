445141c84e157c84c290b4c199aaa5459b9d4174
erinestafford <erinestafford@gmail.com>
2016-06-08 10:03:53 -0500

    optimizing to fit end of real data graph

--- SIR model/real_model_cmp.m
+++ SIR model/real_model_cmp.m
@@ -2,6 +2,7 @@ function [out] = real_model_cmp(model, infected_real)
 %real_model_cmp gets norm of difference
 
 difference = model(1:length(infected_real),4) - infected_real';
-out = sum((difference'.^2));
+flat = model(20:length(infected_real),4) - infected_real(20:end)';
+out = sum((difference'.^2)) + sum((flat'.^2));
 end
 
