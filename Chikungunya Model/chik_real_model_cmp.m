84764ec3dfac824dee477f71a431c9f222c05e8c
emeyer121 <emeyer121@gmail.com>
2016-06-08 11:09:56 -0500

    fix function

--- Chikungunya Model/chik_real_model_cmp.m
+++ Chikungunya Model/chik_real_model_cmp.m
@@ -1,5 +1,7 @@
 function [difference] = chik_real_model_cmp(model, infected_real)
 %real_model_cmp gets norm of difference
 difference = norm(model(1:length(infected_real),5) - infected_real');
+flat = model(20:length(infected_real),5) - infected_real(20:end)';
+out = sum((difference'.^2)) + sum((flat'.^2));
 
 end
