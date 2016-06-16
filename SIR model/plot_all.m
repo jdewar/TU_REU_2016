c8f0f9ecdda007e57a8ea4d98a26f23ac0686b22
emeyer121 <emeyer121@gmail.com>
2016-06-16 11:32:07 -0500

    plot all confirmed

--- SIR model/plot_all.m
+++ SIR model/plot_all.m
@@ -3,8 +3,8 @@ db = create_database();
     for i = 1:50
         data = db(i);
         figure(i)
-        plot_data(data.count);
-        count = data.count_linear;
+        count = data.count_confirmed;
+        plot_data(count);
         str = sprintf('%s %g',data.name, (count(end)/data.population));
         title(str);
         
