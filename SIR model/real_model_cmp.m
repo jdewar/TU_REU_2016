4096d765b74015ddef3c616cab866bba0aaa6452
erinestafford <erinestafford@gmail.com>
2016-06-16 14:20:49 -0500

    removing flat part from optimization

--- SIR model/real_model_cmp.m
+++ SIR model/real_model_cmp.m
@@ -1,9 +1,7 @@
 function [out] = real_model_cmp(model, infected_real)
 %real_model_cmp gets norm of difference
 difference = model(:,4) - infected_real';
-% flat = model(20:length(infected_real),4) - infected_real(20:end)';
-
-out = sum((difference'.^2));% + sum((flat'.^2));
+out = sum((difference'.^2));
 
 end
 
