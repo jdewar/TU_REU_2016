8688d601ed22a2be34d20b5eecc1d664ceabcc4f
erinestafford <erinestafford@gmail.com>
2016-06-16 11:32:07 -0500

    assuming model is the same length as the data

--- SIR model/plot_both.m
+++ SIR model/plot_both.m
@@ -5,8 +5,8 @@ xlabel('Time in Weeks','fontsize',16)
 ylabel('Population','fontsize',16)
 hold on
 t1 = t';
-plot(t1(1:length(data)),data, '*');
-plot(t1(1:length(data)),Y(1:length(data),4), 'b')
+plot(t1,data, '*');
+plot(t1,Y(:,4), 'b')
 legend('Real infected count','Model infected count')
 
 
--- SIR model/real_model_cmp.m
+++ SIR model/real_model_cmp.m
@@ -1,8 +1,9 @@
 function [out] = real_model_cmp(model, infected_real)
 %real_model_cmp gets norm of difference
+difference = model(:,4) - infected_real';
+% flat = model(20:length(infected_real),4) - infected_real(20:end)';
 
-difference = model(1:length(infected_real),4) - infected_real';
-flat = model(20:length(infected_real),4) - infected_real(20:end)';
-out = sum((difference'.^2)) + sum((flat'.^2));
+out = sum((difference'.^2));% + sum((flat'.^2));
+
 end
 
