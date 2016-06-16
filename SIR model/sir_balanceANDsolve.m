6af917bb632a507aadd9acedfae68c9e961c1e9c
erinestafford <erinestafford@gmail.com>
2016-06-16 14:21:41 -0500

    balancing to cumulative infected

--- SIR model/sir_balanceANDsolve.m
+++ SIR model/sir_balanceANDsolve.m
@@ -1,13 +1,14 @@
 function [t,out] = sir_balanceANDsolve(t_in, init, param)
        balance_init = init;
-       balance_init(2) = .001;
+       balance_init(2) = .0001;
+       balance_init(4) = balance_init(2);
        balance_init(1) = balance_init(1) + init(2) - balance_init(2);
    
        
-       options = odeset('Events',@(t,Y)sir_balancing_event(t, Y, init(2))); 
+       options = odeset('Events',@(t,Y)sir_balancing_event(t, Y, init(4))); 
        [t,Y, te, ye, ie] = sir_output([0 1000], balance_init, param, options);
        if numel(te) == 0 && R0_calc(param) > 1
-           [Y(end, 2), R0_calc(param)]
+           [Y(end, 2), R0_calc(param)];
        end
        new_init = Y(end,:)';
        %new_init(4) = new_init(2);
--- SIR model/sir_balancing_event.m
+++ SIR model/sir_balancing_event.m
@@ -1,6 +1,6 @@
 function [value,isterminal,direction] = sir_balancing_event(t,out, init_infected)
 %balancing_event gives correct initial conditions
-value = out(2) - init_infected; %triggers event when infected ==1
+value = out(4) - init_infected; %triggers event when infected ==1
 isterminal = 1; %stops graph when event is triggered
 direction = 1; 
 end
