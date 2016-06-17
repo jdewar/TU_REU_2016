f4466bd9b239ee2947099f75cf34a02cbaaa9c95
erinestafford <erinestafford@gmail.com>
2016-06-17 15:24:16 -0500

    minimum R0 in objective function changed from 1 to 1.00001

--- Chikungunya Model/chik_obj_fn.m
+++ Chikungunya Model/chik_obj_fn.m
@@ -3,9 +3,10 @@ function val = chik_obj_fn(param_array, data, array_names, t_in, functions)
 
 params = array2struct(param_array, array_names);
 
-c = 1 - chik_calc_R0(params, functions, t_in(1));
+c = 1.00001 - chik_calc_R0(params, functions, t_in(1));
 
 new_init = chik_init_conditions(params, t_in);
+
 [~,Y] = chik_balanced_solve(t_in, new_init, params, functions); 
  val = chik_cmp_real_model(Y, data);
 
