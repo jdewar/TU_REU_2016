2225666076aaccb3fdd10ce458e103bb02190bf3
erinestafford <erinestafford@gmail.com>
2016-06-17 15:23:19 -0500

    span and model same length as real data

--- Chikungunya Model/chik_cmp_real_model.m
+++ Chikungunya Model/chik_cmp_real_model.m
@@ -1,7 +1,6 @@
 function [out, difference] = chik_cmp_real_model(model, infected_real)
 %CHIK_CMP_REAL_MODEL returns sum squared difference of model and data
-
-difference = model(1:length(infected_real),5) - infected_real';
+difference = model(:,5) - infected_real';
 
 out = sum((difference.^2));
 
--- Chikungunya Model/chik_plot_both.m
+++ Chikungunya Model/chik_plot_both.m
@@ -5,8 +5,8 @@ xlabel('Time in weeks')
 ylabel('population')
 hold on
 t1 = t'./7;
-plot(t1(1:length(data)),data, '*');
-plot(t1(1:length(data)),Y(1:length(data),5), 'b')
+plot(t1,data, '*');
+plot(t1,Y(:,5), 'b')
 legend('Real infected count','Model infected count')
 
 
