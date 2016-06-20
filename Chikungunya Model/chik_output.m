3d123ff7993fbf5e1a47081954112c7f7d3aa7d7
erinestafford <erinestafford@gmail.com>
2016-06-20 14:18:28 -0500

    had to use different solver for some data

--- Chikungunya Model/chik_output.m
+++ Chikungunya Model/chik_output.m
@@ -3,10 +3,10 @@ function [t,out, te,ye,ie] = chik_output(t_in, Y, param, options, functions)
 dydt_fn = @(t,Y) chikungunya_rhs(t, Y, param, functions);
 
 if numel(options) ~= 0
-    [t, out, te, ye, ie] = ode23(dydt_fn, t_in, Y, options);
+    [t, out, te, ye, ie] = ode23s(dydt_fn, t_in, Y, options);
 else
     
-    [t, out] = ode23(dydt_fn, t_in, Y);
+    [t, out] = ode23s(dydt_fn, t_in, Y);
 end
 
 end
