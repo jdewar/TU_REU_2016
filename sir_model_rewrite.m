7d7ff78d106231cd5684aaadab12c02e04a2794b
emeyer121 <emeyer121@gmail.com>
2016-06-20 09:43:06 -0500

    commented everything

--- sir_model_rewrite.m
+++ sir_model_rewrite.m
@@ -30,17 +30,17 @@ param_array = {param.beta, param.c, param.gamma, param.init_cumu_inf};
  
  
 %% Initial Conditions
-init = [pop - init_cumu_inf, init_cumu_inf, 0, init_cumu_inf];
+init = [pop - init_cumu_inf, init_cumu_inf, 0, init_cumu_inf]; %initial conditions [susceptible, infected, recovered, cumulative infected]
 
 
 %% Plot Unbalanced SIR Dyanmics
 figure ()
-[t, soln] = solve_rhs_equations (tdata, init, param, []);
+[t, soln] = solve_rhs_equations (tdata, init, param, []); %calls ode45 to solve odes
 hold on
-plot(t, soln(:,1), 'g');
-plot(t, soln(:,2), 'k');
-plot(t, soln(:,3), 'b');
-plot(t, soln(:,4), 'r');
+plot(t, soln(:,1), 'g'); %plot susceptible
+plot(t, soln(:,2), 'k'); %plot infected
+plot(t, soln(:,3), 'b'); %plot recovered
+plot(t, soln(:,4), 'r'); %plot cumulative infected
 title('SIR Unbalanced Population Dynamics')
 xlabel('Time')
 ylabel('Population')
@@ -48,24 +48,24 @@ legend('Susceptible', 'Infected', 'Recovered', 'Cumulative Infected')
 
 
 %% Balancing
-bal_init = init;
-bal_init(2) = .001;
-bal_init(4) = bal_init(2);
-bal_init(1) = init(1) + init(2) - bal_init(2);
+bal_init = init; %start with setting balanced init to original initial conditions
+bal_init(2) = .0001; %set initial infected to small number
+bal_init(4) = bal_init(2); %set initial cumulative infected to initial infected
+bal_init(1) = init(1) + init(2) - bal_init(2); %set susceptible to (pop - inf) + inf - small number
 
-options = odeset('Events',@(time, out)balancing_function(time, out, init_cumu_inf));
-[t, Y] = solve_rhs_equations(tdata, bal_init, param, options);
-new_init = Y(end, :);
-[t, bal_soln] = solve_rhs_equations(tdata, new_init, param, []);
+options = odeset('Events',@(time, out)balancing_function(time, out, init_cumu_inf)); %use events function, calls balancing function
+[t, Y] = solve_rhs_equations(tdata, bal_init, param, options); %calls solver to solve odes with new initial conditions
+new_init = Y(end, :); %when event is triggered, endpoints become new_init
+[t, bal_soln] = solve_rhs_equations(tdata, new_init, param, []); %solve again, starting with new_init and output balanced solution
 
 
 %% Plot Balanced SIR Dynamics
 figure ()
 hold on
-plot(t, bal_soln(:,1), 'g');
-plot(t, bal_soln(:,2), 'k');
-plot(t, bal_soln(:,3), 'b');
-plot(t, bal_soln(:,4), 'r');
+plot(t, bal_soln(:,1), 'g'); %plot susceptible
+plot(t, bal_soln(:,2), 'k'); %plot infected
+plot(t, bal_soln(:,3), 'b'); %plot recovered
+plot(t, bal_soln(:,4), 'r'); %plot cumulative infected
 title('SIR Balanced Population Dynamics')
 xlabel('Time')
 ylabel('Population')
@@ -73,31 +73,32 @@ legend('Susceptible', 'Infected', 'Recovered', 'Cumulative Infected')
 
 
 %% Optimizing
-lb = [1, 0.001, 0.001, 0.01];
+lb = [1, 0.001, 0.001, 0.01]; %boundaries for parameters [beta, c, gamma, init_cumu_inf]
 ub = [1,   200,   200, mean(count)* .95];
-half = (lb + ub) / 2;
-opt_func = @(param_array)objective_function(bal_soln, count, param, array_names, param_array, tdata, new_init);
-options = optimset('Algorithm', 'sqp');
+half = (lb + ub) / 2; %set this as starting point for optimization
+opt_func = @(param_array)objective_function(soln, count, param, array_names, param_array, tdata, init); %call obj fn with inputs
+options = optimset('Algorithm', 'sqp'); %choose some way of optimizing
 param
 init_cumu_inf
 
-[new_param] = fmincon(opt_func, half, [], [], [], [], lb, ub, [], options);
+[new_param] = fmincon(opt_func, half, [], [], [], [], lb, ub, [], options); %fmincon optimizes using obj fn
 
+%set parameters to new values
 param.beta = new_param(1);
 param.c = new_param(2);
 param.gamma = new_param(3);
 param.init_cumu_inf = new_param(4);
 param
 
-new_init = [pop - param.init_cumu_inf, param.init_cumu_inf, 0, param.init_cumu_inf]
+new_init = [pop - param.init_cumu_inf, param.init_cumu_inf, 0, param.init_cumu_inf]; %don't think this is necessary
 
-[tdata, opt_soln] = solve_rhs_equations(tdata, new_init, param, []);
+[tdata, opt_soln] = solve_rhs_equations(tdata, new_init, param, []); %solve with new parameters and initial conditions
 
 
 %% Plot Optimized Model & Data
 figure ()
 hold on
-plot(tdata, opt_soln(:,4), 'r');
+plot(tdata, opt_soln(:,4), 'r'); %plot cumu inf with data
 plot(count, '*')
 title(name)
 xlabel('Time')
@@ -126,9 +127,9 @@ end
 
 %% Solve RHS Equations
 function [t, soln] = solve_rhs_equations (time, init, param, options)
-if numel(options) ~= 0
+if numel(options) ~= 0 %if solving with options, output te, ye, ie
 [t, soln, te, ye, ie] = ode45(@(time, init)rhs_equations(time, init, param), time, init, options);
-else
+else %otherwise, solve normally
 [t, soln] = ode45(@(time, init)rhs_equations(time, init, param), time, init, options);
 end
 end
@@ -142,9 +143,9 @@ end
     
 %% Balancing Function
 function [value, isterminal, direction] = balancing_function(time, out, init_cumu_inf)
-value = out(4) - init_cumu_inf;
-isterminal = 1;
-direction = 1;
+value = out(4) - init_cumu_inf; %set function to trigger when init cumu rhs equation is equal to init cumu from data
+isterminal = 1; %stops integrating when value = 0
+direction = 1; %event happens when function increasing
 end
 
 
@@ -152,18 +153,18 @@ end
 function [difference] = objective_function (bal_soln, count, param, array_names, param_array, tdata, new_init)
     for i = 1:length(array_names)
         name = array_names{i}; 
-        param.(name) = param_array(i);
+        param.(name) = param_array(i); %change parameters from struct to array
     end
     
-[t, out] = solve_rhs_equations (tdata, new_init, param, []);
-squared_diff = (out(:,4) - (count)').^2;
-difference = sum(squared_diff);
+[t, out] = solve_rhs_equations (tdata, new_init, param, []); %call solutions from solver
+squared_diff = (out(:,4) - (count)').^2; %squared difference between model and data
+difference = sum(squared_diff); %take sum of squared diff, this value is what is being optimized
 
-R0 = calculate_R0 (param);
-c = 1.0001 - R0;
+R0 = calculate_R0 (param); 
+c = 1.0001 - R0; %need R0>1, need c to be negative
 
     if c>0
-        difference = difference * (1+c);
+        difference = difference * (1+c); %if R0 is less than 1, add linear portion to optimizer so it is not flat
     end
     
 end
