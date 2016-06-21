0ff182b8e6c60f5441f83f450f4a0ce542d143c7
erinestafford <erinestafford@gmail.com>
2016-06-21 13:00:29 -0500

    changed get data function in plot all

--- Chikungunya Model/plot_all_newly_infected.m
+++ Chikungunya Model/plot_all_newly_infected.m
@@ -6,21 +6,22 @@ country_names = {db.name};
     for i = 1:50
         [data2014, ~, name, firstWeek2014] = data_from_entry(db(i));
         tspan = (firstWeek2014*7):7:((length(data2014)+firstWeek2014-1)*7); % tspan not size of real data
-        
         for j = 1:length(no_data_2015)
            if strcmp(name, no_data_2015{j}) == 1
                newly_infected = get_newly_infected_count(data2014);
+               sum_data = sum_data + data2014;
                hold all
                chik_plot_NewlyInfected(tspan,newly_infected)
                str = sprintf('%s',name);
                title(str);
            else
-               [data2015] = get_data2015(country_names{i});
+               [data2015] = data_from_idx_2015(db2015, i);
                tspan_full_count = (firstWeek2014*7):7:((length(data2015)+length(data2014)+firstWeek2014-1)*7);
         
                newly_infected = get_newly_infected_count(data2015);
               
                full_count = combine_data(data2014,newly_infected);
+
         
                newly_infected_combined = get_newly_infected_count(full_count);
        
@@ -30,6 +31,7 @@ country_names = {db.name};
            end
         end
         
+
         
     end
 
