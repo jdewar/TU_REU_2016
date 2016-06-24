3b9fb5e712c167fb58b857849ecb554ef75b6e63
erinestafford <erinestafford@gmail.com>
2016-06-24 09:38:38 -0500

    plotting predictions SIR

--- SIR model/sir_model.m
+++ SIR model/sir_model.m
@@ -4,77 +4,73 @@ addpath('../data');
 country = 'Dominica Republic';
 [real_full, pop] = get_data(country);
 real = real_full(1:25);
-[real2015] = get_data2015(country);
+full_count = combine_data(country);
+init_infected = real(1);
+tend = length(real);
+total_pop = pop;
+tspan = [1:tend];
+%tspan_predictions = [1:length(full_count)];
+tspan_predictions = [1:length(real_full)];
+close all;
+%parameters
+field1 = 'beta';  value1 = 1;
+field2 = 'c';  value2 = 1;
+field3 = 'gamma';  value3 = 1/2;
+field4 = 'init_cumu_infected'; value4 = init_infected;
+
+param = struct(field1,value1,field2,value2,field3,value3,field4,value4);
+
+%initial conditions
+%init = sir_init_conditions(param, tspan, total_pop);
+init = [1000,1,0,1];
+array_names = {'beta','c','gamma','init_cumu_infected'};
+param_array = [param.beta,param.c,param.gamma,param.init_cumu_infected];
+
+%% solving
+
+%figure()
+% [t_model,out_model] = sir_balanceANDsolve([0 1000], init, param);
+% R0 = (param.beta*param.c)/param.gamma
+% plot_sir_model(t_model,out_model)
+
+%  figure()
+%  plot_data(real)
+%  drawnow
+fn = @(x)sir_obj_fn(x,real,param,array_names,tspan_predictions,total_pop);
+lb = [1,0.001,0.001,.01];
+ub = [1,200,200,mean(real)* .95];
+
+half = (lb+ub)/2;
+options = optimset('Algorithm', 'sqp');
+[parray] = fmincon(fn, half, [],[],[],[],lb,ub, [], options);
+
+val = sir_obj_fn(param_array,real, param,array_names,tspan,total_pop);
+
+param.beta = parray(1);
+param.c = parray(2);
+param.gamma = parray(3);
+param.init_cumu_infected = parray(4);
+
+% %Check that R0 is greater than 1
+% R0 = (param.beta*param.c)/param.gamma;
+% if (R0 <=1)
+%     display('R0 is less than 1')
+%     return
+% end
+
+
+new_init = sir_init_conditions(param, tspan_predictions, total_pop);
+[t,out] = sir_balanceANDsolve(tspan_predictions, new_init, param);
+
 figure()
-plot(1:length(real2015), real2015, '*');
-drawnow
-% full_count = combine_data(country);
-% init_infected = real(1);
-% tend = length(real);
-% total_pop = pop;
-% tspan = [1:tend];
-% %tspan_predictions = [1:length(full_count)];
-% tspan_predictions = [1:length(real_full)];
-% close all;
-% %parameters
-% field1 = 'beta';  value1 = 1;
-% field2 = 'c';  value2 = 1;
-% field3 = 'gamma';  value3 = 1/2;
-% field4 = 'init_cumu_infected'; value4 = init_infected;
-% 
-% param = struct(field1,value1,field2,value2,field3,value3,field4,value4);
-% 
-% %initial conditions
-% %init = sir_init_conditions(param, tspan, total_pop);
-% init = [1000,1,0,1];
-% array_names = {'beta','c','gamma','init_cumu_infected'};
-% param_array = [param.beta,param.c,param.gamma,param.init_cumu_infected];
-% 
-% %% solving
-% 
-% %figure()
-% % [t_model,out_model] = sir_balanceANDsolve([0 1000], init, param);
-% % R0 = (param.beta*param.c)/param.gamma
-% % plot_sir_model(t_model,out_model)
-% 
-% %  figure()
-% %  plot_data(real)
-% %  drawnow
-% fn = @(x)sir_obj_fn(x,real,param,array_names,tspan_predictions,total_pop);
-% lb = [1,0.001,0.001,.01];
-% ub = [1,200,200,mean(real)* .95];
-% 
-% half = (lb+ub)/2;
-% options = optimset('Algorithm', 'sqp');
-% [parray] = fmincon(fn, half, [],[],[],[],lb,ub, [], options);
-% 
-% val = sir_obj_fn(param_array,real, param,array_names,tspan,total_pop);
-% 
-% param.beta = parray(1);
-% param.c = parray(2);
-% param.gamma = parray(3);
-% param.init_cumu_infected = parray(4);
-% 
-% % %Check that R0 is greater than 1
-% % R0 = (param.beta*param.c)/param.gamma;
-% % if (R0 <=1)
-% %     display('R0 is less than 1')
-% %     return
-% % end
-% 
-% 
-% new_init = sir_init_conditions(param, tspan_predictions, total_pop);
-% [t,out] = sir_balanceANDsolve(tspan_predictions, new_init, param);
-% 
-% figure()
-% %plot_both(tspan_predictions,out,full_count);
-% %hold on
-% %plot([tend,tend], [0,max(full_count)]);
-% plot_both(tspan_predictions,out,real_full);
-% hold on
-% plot([tend,tend], [0,max(real_full)]);
-% 
-% 
+%plot_both(tspan_predictions,out,full_count);
+%hold on
+%plot([tend,tend], [0,max(full_count)]);
+plot_both(tspan_predictions,out,real_full);
+hold on
+plot([tend,tend], [0,max(real_full)]);
+
+
 % % %  
 % % % figure(2)
 % % % range = linspace(lb(4),ub(4), 40);
