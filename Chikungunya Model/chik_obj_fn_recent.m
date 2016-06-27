805a63bffab80c4cd76c218428e24dc5aa040940
emeyer121 <emeyer121@gmail.com>
2016-06-27 10:33:39 -0500

    objective functions adding last five weeks

--- Chikungunya Model/chik_obj_fn_recent.m
+++ Chikungunya Model/chik_obj_fn_recent.m
@@ -1,4 +1,4 @@
-function [val_real,val_recent] = chik_obj_fn(param_array, data, array_names, t_in, functions)
+function val = chik_obj_fn(param_array, data, array_names, t_in, functions)
 %chik_obj_fn returns the value that the objective function optimizes over
 
 params = array2struct(param_array, array_names);
@@ -8,15 +8,13 @@ c = 1.00001 - chik_calc_R0(params, functions, t_in(1));
 new_init = chik_init_conditions(params, t_in);
 
 [~,Y] = chik_balanced_solve(t_in, new_init, params, functions); 
-val_recent = chik_cmp_recent(Y, data); 
-val_real = chik_cmp_real_model(Y, data);
-
+ val = chik_cmp_recent(Y, data);
 
  
 %forces optimizer to run over smooth curve when R0 < 1.5 by multiplying by 
 %the distance from minimum R0 
 if c>0
-     val_real = val_real * (1+c);
+     val = val * (1+c);
   
 end
 
