de97acef20e21eb8bbdebbdd64ce122d9240f3d0
emeyer121 <emeyer121@gmail.com>
2016-12-07 11:42:28 -0600

    added figures, fixed Reff

--- Risk Based Model/Two Groups Behavior Change/plot_Reff.m
+++ Risk Based Model/Two Groups Behavior Change/plot_Reff.m
@@ -1,8 +1,12 @@
 function[Reff] = plot_Reff(t,Y,p)
+figure()
 Reff = zeros(size(t));
  i = 1;
 while i <= length(t)
    Reff(i) = calc_Reff(p,transpose(Y(i,:)));
     i = i + 1;
 end
-plot(t./7,Reff)
+plot(t, Reff, 'b', 'linewidth', 2)
+xlabel('Time')
+ylabel('Effective Reproductive Number')
+title('R_e over Time')
