1a8832aeeab46336f3db6b1578ac493cc1725a51
erinestafford <erinestafford@gmail.com>
2016-12-12 07:13:28 -0600

    adding rho

--- Risk Based Model/Two Groups Behavior Change/calc_b_T.m
+++ Risk Based Model/Two Groups Behavior Change/calc_b_T.m
@@ -1,4 +1,4 @@
-function [ b_T ] = calc_b_T(P, init)
+function [ b_T,rho_h, rho_v ] = calc_b_T(P, init)
 %c1 = get_init_conditions1(P);
 %c2 = get_init_conditions2(P);
 
@@ -10,6 +10,7 @@ b_vw = P.sigma_v * init(4) + P.sigma_v * init(5) + P.sigma_v * init(6);
 
 %total bites
 b_T = (b_hw * b_vw)/(b_hw + b_vw);
-
+rho_h = b_hw/b_T;
+rho_v = b_vw/b_T;
 end
 
