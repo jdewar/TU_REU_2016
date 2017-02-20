9558a65661818385aa7bf57d30235b9b134995d7
erinestafford <erinestafford@gmail.com>
2017-02-20 14:07:04 -0600

    change main function back

--- Risk Based Model/Two Groups Behavior Change/twoRiskGroup.m
+++ Risk Based Model/Two Groups Behavior Change/twoRiskGroup.m
@@ -32,15 +32,27 @@ param_struct = ...
      'H0', pop;
      'theta1', .3; %proportion of population in group 1 - low risk
      'theta2', .7;% proportion of population in group 2 - high risk
-     'theta0', 0;
+     'theta0', 1;
      'init_cumulative_infected', init_infected_h;
      'K_v' , pop * 2;
-     'pi1', 1; %proportion that continues to be bitten in infected group 1
-     'pi2', 1; %proportion that continues to be bitten in infected group 2
+     'pi1', 0.4; %proportion that continues to be bitten in infected group 1
+     'pi2', 0.4; %proportion that continues to be bitten in infected group 2
     }';
 params = struct(param_struct{:});
 array_names = param_struct(1,:);
 params.theta0 = 1 - (params.theta1 + params.theta2);
+h = 100:10:10000;
+v = 1000:100:1000000;
+g = @(x,y) (x/y)/(1+(x/y));
+k = 1:10000;
+
+for i = h
+    for j = v
+        f(k) = g(i,j);
+        l(k) = i/j;
+    end
+end
+plot(l,f)
 %% Plot Cumulative vs. Newly Infected
 %  figure()
 %  subplot(1,2,1)
@@ -97,58 +109,60 @@ init = ...
     0];
 %params.theta2 = 0.2;
 %params.pi2 = 0;
-% figure()
-% [t,out] = output([0:40], init, params, []);
-% plot_modelu(t, out);
-
-% params
-% [t_model,out_model] = balance_and_solve([0:40], init, params);
-% R01 = calc_R0(params, out_model(1,:))
-% Reff = calc_Reff(params, out_model(1,:));
-% bT = calc_b_T(params, init)
+params
+m = 1;
+h = .001:.001:1;
+rho
+for i = h
+    [t_model,out_model] = balance_and_solve([0:400], init, params);
+    R01 = calc_R0(params, out_model(1,:))
+    Reff = calc_Reff(params, out_model(1,:));
+    b_T,rho_h, rho_v  = calc_b_T(params, init)
+end
 % [peak] = get_peak_infected(out_model)
 % total = out_model(end,7) + out_model(end,8)
 % plot_Reff(t_model,out_model,params);
 % plot_model(t_model,out_model);
-% sensitivity_time(params, out_model, t_model)
+% %sensitivity_time(params, out_model, t_model)
 
 %% Optimization & Plot - Original Obj Fn
-lb = struct2array(params,array_names);
-ub = struct2array(params,array_names);
-
-%  [lb, ub] = range(lb, ub, 'sigma_h1', .1, 5, array_names);
-%  [lb, ub] = range(lb, ub, 'sigma_h2', 5, 50, array_names);
- [lb, ub] = range(lb, ub, 'theta1', .001, .4, array_names);
- [lb, ub] = range(lb, ub, 'theta2', .4, .6, array_names);
- [lb, ub] = range(lb, ub, 'init_cumulative_infected', params.init_cumulative_infected * 0.1, params.init_cumulative_infected * 10, array_names);
- [lb, ub] = range(lb, ub, 'K_v', params.H0, params.H0 * 10, array_names);
- [lb, ub] = range(lb, ub, 'pi1', .01, 1, array_names);
- [lb, ub] = range(lb, ub, 'pi2', .01, 1, array_names);
- [lb, ub] = range(lb, ub, 'H0', params.H0 *0.1, params.H0, array_names);
-
-%  I* from integrating steady state
-c = 1;
-for i = 1:length(lb)
-    if lb(i) ~= ub(i)
-        optimized{c} = array_names{i};
-        c = c+1;
-    end
-end
-optimized;
-
-obj_fn1 = @(parray)obj_fn(parray, real, array_names, tspan, get_init_conditions(params, tspan));
-[opt_params1,fval,grad,hes] = optimizer(obj_fn1, lb, ub, params);
-opt_params1.theta0 = 1-(opt_params1.theta1+ opt_params1.theta2);
-opt_params1
-
-init1 = get_init_conditions(opt_params1, tspan);
-[t1,out1] = balance_and_solve([0 tspan], init1, opt_params1);
-peak = get_peak_infected(out1);
-
-figure()
-plot_both(t1, out1, real);
-drawnow
-
+% lb = struct2array(params,array_names);
+% ub = struct2array(params,array_names);
+% 
+% %  [lb, ub] = range(lb, ub, 'sigma_h1', .1, 5, array_names);
+% %  [lb, ub] = range(lb, ub, 'sigma_h2', 5, 50, array_names);
+%  [lb, ub] = range(lb, ub, 'theta1', .001, .4, array_names);
+%  [lb, ub] = range(lb, ub, 'theta2', .4, .6, array_names);
+%  [lb, ub] = range(lb, ub, 'init_cumulative_infected', params.init_cumulative_infected * 0.1, params.init_cumulative_infected * 10, array_names);
+%  [lb, ub] = range(lb, ub, 'K_v', params.H0, params.H0 * 10, array_names);
+%  [lb, ub] = range(lb, ub, 'pi1', .01, 1, array_names);
+%  [lb, ub] = range(lb, ub, 'pi2', .01, 1, array_names);
+%  [lb, ub] = range(lb, ub, 'H0', params.H0 *0.1, params.H0, array_names);
+% 
+% %  I* from integrating steady state
+% c = 1;
+% for i = 1:length(lb)
+%     if lb(i) ~= ub(i)
+%         optimized{c} = array_names{i};
+%         c = c+1;
+%     end
+% end
+% optimized;
+% 
+% obj_fn1 = @(parray)obj_fn(parray, real, array_names, tspan, get_init_conditions(params, tspan));
+% [opt_params1,fval,grad,hes] = optimizer(obj_fn1, lb, ub, params);
+% opt_params1.theta0 = opt_params1.theta1+ opt_params1.theta2;
+% opt_params1
+% real;
+%  
+% init1 = get_init_conditions(opt_params1, tspan);
+% [t1,out1] = balance_and_solve([0 tspan], init1, opt_params1);
+% peak = get_peak_infected(out1);
+% 
+% figure()
+% plot_both(t1, out1, real);
+% drawnow
+% 
 %  R01 = calc_R0(opt_params1, out1(1,:))
 % 
 % params.H0 = 1000;
