a09c2b66deb5d507326ff2ec98e62336ce081724
emeyer121 <emeyer121@gmail.com>
2017-03-07 15:46:30 -0600

    this was wrong, thanks jeremy

--- Risk Based Model/Two Groups Behavior Change/balancing_event.m
+++ Risk Based Model/Two Groups Behavior Change/balancing_event.m
@@ -2,7 +2,7 @@ function [value,isterminal,direction] = balancing_event(t, Y, init7, init8)
 %CHIK_BALANCING_EVENT event halting at when reaching initial infected
 % infected at desired level
 
-if (Y(7) + Y(8)) - (init7 + init8) < abs(0.01)
+if abs((Y(7) + Y(8)) - (init7 + init8)) < 0.01
     value = 0;
 else
     value = (Y(7) + Y(8)) - (init7 + init8); % dI/dt = 0
